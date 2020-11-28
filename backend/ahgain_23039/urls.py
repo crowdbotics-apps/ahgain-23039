6e7322b70b77f015608a6e8a287839c711dab4f4
Crowdbotics <team@crowdbotics.com>
2020-11-28 18:38:47 +0000

    Update models

--- backend/ahgain_23039/urls.py
+++ backend/ahgain_23039/urls.py
@@ -32,6 +32,8 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
     path("home/", include("home.urls")),
     path("api/v1/", include("users.api.v1.urls")),
+    path("api/v1/", include("dating.api.v1.urls")),
+    path("dating/", include("dating.urls")),
 ]
 
 admin.site.site_header = "ahgain"
